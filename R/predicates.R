589c5ff81fc7041ba07cf848b44e13d07cd15fd9
Chad <Chad.Goymer@gmail.com>
2019-01-29 15:08:23 +0000

    added is_scalar

--- R/predicates.R
+++ R/predicates.R
@@ -11,3 +11,17 @@
 is_vector <- function(x) {
   !is.null(x) && is.atomic(x) && !is.array(x)
 }
+
+#  FUNCTION: is_scalar ------------------------------------------------------------------------
+#
+#' Checks whether the variable is an atomic vector of length 1
+#'
+#' @param x (any) The object to test
+#'
+#' @return TRUE if x is an atomic vector of length 1, FALSE otherwise
+#'
+#' @export
+#'
+is_scalar <- function(x) {
+  is_vector(x) && identical(length(x), 1L)
+}
--- tests/testthat/test-predicates.R
+++ tests/testthat/test-predicates.R
@@ -26,3 +26,28 @@ test_that("is_vector returns FALSE for invalid vectors", {
   expect_false(is_vector(NULL))
 
 })
+
+# TEST: is_scalar -----------------------------------------------------------------------------
+
+test_that("is_scalar returns TRUE for valid vectors", {
+
+  expect_true(is_scalar(TRUE))
+  expect_true(is_scalar(1L))
+  expect_true(is_scalar(3.142))
+  expect_true(is_scalar("bob"))
+
+})
+
+test_that("is_scalar returns FALSE for invalid vectors", {
+
+  expect_false(is_scalar(c(TRUE, FALSE)))
+  expect_false(is_scalar(1:3))
+  expect_false(is_scalar(c(3.142, 2.718, 1.414)))
+  expect_false(is_scalar(c("bob", "jane", "jim")))
+
+  expect_false(is_scalar(list(1)))
+  expect_false(is_scalar(matrix(1, nrow = 1)))
+  expect_false(is_scalar(data.frame(x = "A")))
+  expect_false(is_scalar(NULL))
+
+})
