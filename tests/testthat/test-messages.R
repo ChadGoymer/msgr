552f71c7d1e0654c53dc352247f2a01c5dc7c35c
Chad <Chad.Goymer@gmail.com>
2019-02-18 18:57:06 +0000

    added warn()

--- tests/testthat/test-messages.R
+++ tests/testthat/test-messages.R
@@ -64,3 +64,68 @@ test_that("invalid arguments for info throw an error", {
     "'log_path' must be a string: 0")
 
 })
+
+# TEST: warn ----------------------------------------------------------------------------------
+
+test_that("warn displays a warning, and records it in a log file", {
+
+  log_dir <- tempdir()
+  log_path <- file.path(log_dir, "test-warn.log")
+
+  expect_warning(warn("This is a WARNING", log_path = log_path), "This is a WARNING")
+  expect_true(file.exists(log_path))
+
+  log_txt <- readLines(log_path)
+  expect_match(log_txt[length(log_txt)], "WARNING \\| This is a WARNING")
+
+  expect_silent(
+    warn("This is a WARNING", level = 2, msg_types = "WARNING", msg_level = 1, log_path = log_path))
+
+  expect_silent(
+    warn("This is a WARNING", level = 1, msg_types = "ERROR", msg_level = 1, log_path = log_path))
+
+  expect_warning(
+    warn("This is a WARNING", level = 2, msg_types = "WARNING", msg_level = 3, log_path = log_path),
+    "This is a WARNING")
+
+  log_length <- length(readLines(log_path))
+
+  expect_warning(
+    warn("This is a WARNING", level = 1, msg_types = "WARNING", msg_level = 1, log_path = ""),
+    "This is a WARNING")
+
+  expect_identical(length(readLines(log_path)), log_length)
+
+})
+
+test_that("invalid arguments for warn throw an error", {
+
+  expect_error(
+    warn("This is a WARNING", level = "a"),
+    "'level' must be an integer between 1 and 10: a")
+
+  expect_error(
+    warn("This is a WARNING", level = 0),
+    "'level' must be an integer between 1 and 10: 0")
+
+  expect_error(
+    warn("This is a WARNING", msg_types = 0),
+    "'msg_types' must be NULL \\(no messages\\) or a character vector containing \"INFO\", \"WARNING\" or \"ERROR\": 0")
+
+  expect_error(
+    warn("This is a WARNING", msg_types = "BOB"),
+    "'msg_types' must be NULL \\(no messages\\) or a character vector containing \"INFO\", \"WARNING\" or \"ERROR\": BOB")
+
+  expect_error(
+    warn("This is a WARNING", msg_level = "a"),
+    "'msg_level' must be an integer between 1 and 10: a")
+
+  expect_error(
+    warn("This is a WARNING", msg_level = 0),
+    "'msg_level' must be an integer between 1 and 10: 0")
+
+  expect_error(
+    warn("This is a WARNING", log_path = 0),
+    "'log_path' must be a string: 0")
+
+})
