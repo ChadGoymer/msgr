0ee516d66acbda175e93531af8ee9664c41c93eb
Chad Goymer <chad.goymer@gmail.com>
2021-02-15 09:01:33 +0000

    Linted try-catch

--- R/try-catch.R
+++ R/try-catch.R
@@ -1,26 +1,30 @@
-#  FUNCTION: try_catch ------------------------------------------------------------------------
+#  FUNCTION: try_catch ---------------------------------------------------------
 #
 #' Try to evaluate an expressions and capture any messages, warnings or errors
 #'
-#' This function is similar to [tryCatch()], except that, by default, errors are captured
-#' and presented using [error()]. Messages and warnings are not captured by this function.
-#' In addition, a "finally" expression can be specified which is evaluated at the end of
-#' the call no matter the result.
+#' This function is similar to [tryCatch()], except that, by default, errors are
+#' captured and presented using [error()]. Messages and warnings are not
+#' captured by this function.
+#'
+#' In addition, a "finally" expression can be specified which is evaluated at
+#' the end of the call no matter the result.
 #'
 #' @param expr (expression) The expression to evaluate
-#' @param on_error (function, optional) A function describing what to do in the event of a
-#'   error in the above expression. The function must take a single argument, which is the
-#'   [simpleError()]. If missing or `NULL`, errors are not caught. Default: [error()]
-#'   called with the error's message prefixed by the calling function name.
-#' @param finally (expression, optional) An expression to evaluate at the end of the call.
-#'    If missing or `NULL`, nothing is actioned.
+#' @param on_error (function, optional) A function describing what to do in the
+#'   event of a error in the above expression. The function must take a single
+#'   argument, which is the [simpleError()]. If missing or `NULL`, errors are
+#'   not caught. Default: [error()] called with the error's message prefixed by
+#'   the calling function name.
+#' @param finally (expression, optional) An expression to evaluate at the end of
+#'   the call. If missing or `NULL`, nothing is actioned.
 #'
 #' @return The result of the evaluated expression
 #'
-#' @examples
-#' \dontrun{
-#'   try_catch(x <- "foo")
-#'   try_catch(stop("This is an error"))
+#' @examples \dontrun{
+#'
+#' try_catch(x <- "foo")
+#' try_catch(stop("This is an error"))
+#'
 #' }
 #'
 #' @export
@@ -28,8 +32,8 @@
 try_catch <- function(
   expr,
   on_error,
-  finally)
-{
+  finally
+) {
   if (missing(on_error) || is_null(on_error)) {
     if (sys.nframe() > 1) {
       calling_function <- deparse(sys.calls()[[sys.nframe() - 1]][[1]])
@@ -38,9 +42,13 @@ try_catch <- function(
       prefix <- ""
     }
 
-    on_error = function(e) {
+    on_error <- function(e) {
       if (grepl("^\\[[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\\] ", e$message)) {
-        e$message <- sub("^\\[[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\\] ", "", e$message)
+        e$message <- sub(
+          pattern     = "^\\[[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\\] ",
+          replacement = "",
+          x           = e$message
+        )
       }
       error(prefix, e$message)
     }
@@ -55,49 +63,55 @@ try_catch <- function(
   tryCatch(expr, error = on_error, finally = finally)
 }
 
-#  FUNCTION: try_map --------------------------------------------------------------------------
+#  FUNCTION: try_map -----------------------------------------------------------
 #
-#' Apply a function over a vector or list, capturing any errors to display at the end
+#' Apply a function over a vector or list, capturing any errors to display at
+#' the end
 #'
-#' This function is similar to [purrr::map()] except that instead of stopping at the first
-#' error it captures them and continues. If there are any errors it collects them together
-#' and displays them at the end. You have the option to specify a prefix to the error message
-#' using the `msg_prefix` argument.
+#' This function is similar to [purrr::map()] except that instead of stopping at
+#' the first error it captures them and continues. If there are any errors it
+#' collects them together and displays them at the end. You have the option to
+#' specify a prefix to the error message using the `msg_prefix` argument.
 #'
-#' If the mapped function is a long running process `try_map()` can output a warning at the time
-#' an error occurs, but specifying the `warn_level` argument to be greater than 0 (see
-#' [warn()] for more details about message levels. Similarly `error_level` argument specifies
-#' the level of any reported error, as detailed in [error()].
+#' If the mapped function is a long running process `try_map()` can output a
+#' warning at the time an error occurs, but specifying the `warn_level` argument
+#' to be greater than 0 (see [warn()] for more details about message levels.
+#' Similarly `error_level` argument specifies the level of any reported error,
+#' as detailed in [error()].
 #'
-#' If you do not want the function to stop with an error, you can instead return a warning or
-#' info message using the `on_error` argument.
+#' If you do not want the function to stop with an error, you can instead return
+#' a warning or info message using the `on_error` argument.
 #'
-#' Finally, `simplify` and `use_names` allow the user to specify whether to simplify the output
-#' to an atomic vector, if possible, and whether to use the vector input `x` as names to the
-#' resulting list.
+#' Finally, `simplify` and `use_names` allow the user to specify whether to
+#' simplify the output to an atomic vector, if possible, and whether to use the
+#' vector input `x` as names to the resulting list.
 #'
 #' @param x (vector or list) The vector or list to map the function to.
 #' @param f (function) The function to map to the elements of `x`.
 #' @param ... (optional) Extra arguments to supply to f.
-#' @param msg_prefix (string, optional) A message to prefix any resulting error message.
-#' @param warn_level (integer, optional) The level of any warnings about errors encountered.
-#'   If 0 then no warnings are shown. Default: 2.
-#' @param error_level (integer, optional) The level of any resulting errors. Default: 1.
-#' @param on_error (string) The kind of message to produce if there is an error. Either "info",
-#'   "warn", or "error". Default: "error".
-#' @param simplify (boolean, optional) Whether to try to simplify the result of the mapping
-#'   into an atomic vector. Default: FALSE.
-#' @param use_names (boolean, optional) Whether to use 'x' as names in the resulting list. 'x'
-#'   must be a character vector for this to work. Default: TRUE.
+#' @param msg_prefix (string, optional) A message to prefix any resulting error
+#'   message.
+#' @param warn_level (integer, optional) The level of any warnings about errors
+#'   encountered. If 0 then no warnings are shown. Default: 2.
+#' @param error_level (integer, optional) The level of any resulting errors.
+#'   Default: 1.
+#' @param on_error (string) The kind of message to produce if there is an error.
+#'   Either "info", "warn", or "error". Default: "error".
+#' @param simplify (boolean, optional) Whether to try to simplify the result of
+#'   the mapping into an atomic vector. Default: FALSE.
+#' @param use_names (boolean, optional) Whether to use 'x' as names in the
+#'   resulting list. 'x' must be a character vector for this to work. Default:
+#'   TRUE.
 #'
-#' @return If `simplify = FALSE` a list is returned. Otherwise, the function attempts to
-#'   simplify the result to an atomic vector or array.
+#' @return If `simplify = FALSE` a list is returned. Otherwise, the function
+#'   attempts to simplify the result to an atomic vector or array.
 #'
-#' @examples
-#' \dontrun{
+#' @examples \dontrun{
+#'
 #'   test_try_map <- function(x, y) if (x > y) stop("x > y") else x
 #'   try_map(1:3, test_try_map, y = 2)
 #'   try_map(1:3, test_try_map, y = 5)
+#'
 #' }
 #'
 #' @export
@@ -111,8 +125,8 @@ try_map <- function(
   error_level = 1,
   on_error    = "error",
   simplify    = FALSE,
-  use_names   = TRUE)
-{
+  use_names   = TRUE
+) {
   if (missing(msg_prefix)) {
     mapped_function <- as.character(substitute(f))
     if (sys.nframe() > 1) {
@@ -174,7 +188,10 @@ try_map <- function(
       prefix <- paste0("\n'", names(result)[is_error], "': ")
     }
 
-    error_msg <- paste0(prefix, map_chr(result[is_error], "message"), collapse = "\n")
+    error_msg <- paste0(
+      prefix, map_chr(result[is_error], "message"),
+      collapse = "\n"
+    )
     if (!is_null(msg_prefix)) {
       error_msg <- paste0(msg_prefix, "\n", error_msg)
     }
@@ -182,8 +199,9 @@ try_map <- function(
     switch(
       on_error,
       error = error(error_msg, level = error_level),
-      warn  = warn( error_msg, level = error_level),
-      info  = info( error_msg, level = error_level))
+      warn  = warn(error_msg,  level = error_level),
+      info  = info(error_msg,  level = error_level)
+    )
   }
 
   if (simplify && all(map_int(result, length) == 1L)) {
@@ -193,49 +211,56 @@ try_map <- function(
   result
 }
 
-#  FUNCTION: try_pmap -------------------------------------------------------------------------
+#  FUNCTION: try_pmap ----------------------------------------------------------
 #
-#' Apply a function over a list of vectors, capturing any errors to display at the end
+#' Apply a function over a list of vectors, capturing any errors to display at
+#' the end
 #'
-#' This function is similar to [purrr::pmap()] except that instead of stopping at the first
-#' error it captures them and continues. If there are any errors it collects them together
-#' and displays them at the end. You have the option to specify a prefix to the error message
-#' using the `msg_prefix` argument.
+#' This function is similar to [purrr::pmap()] except that instead of stopping
+#' at the first error it captures them and continues. If there are any errors it
+#' collects them together and displays them at the end. You have the option to
+#' specify a prefix to the error message using the `msg_prefix` argument.
 #'
-#' If the mapped function is a long running process `try_pmap` can output a warning at the time
-#' an error occurs, but specifying the `warn_level` argument to be greater than 0 (see
-#' [warn()] for more details about message levels. Similarly `error_level` argument specifies
-#' the level of any reported error, as detailed in [error()].
+#' If the mapped function is a long running process `try_pmap` can output a
+#' warning at the time an error occurs, but specifying the `warn_level` argument
+#' to be greater than 0 (see [warn()] for more details about message levels.
+#' Similarly `error_level` argument specifies the level of any reported error,
+#' as detailed in [error()].
 #'
-#' If you do not want the function to stop with an error, you can instead return a warning or
-#' info message using the `on_error` argument.
+#' If you do not want the function to stop with an error, you can instead return
+#' a warning or info message using the `on_error` argument.
 #'
-#' Finally, `simplify` and `use_names` allow the user to specify whether to simplify the output
-#' to an atomic vector, if possible, and whether to use the vector input `x` as names to the
-#' resulting list.
+#' Finally, `simplify` and `use_names` allow the user to specify whether to
+#' simplify the output to an atomic vector, if possible, and whether to use the
+#' vector input `x` as names to the resulting list.
 #'
 #' @param l (list) A list of vectors the same length to apply the function to.
-#' @param f (function) The function to map to the elements of the vectors in `l`.
+#' @param f (function) The function to map to the elements of the vectors in
+#'   `l`.
 #' @param ... (optional) Extra arguments to supply to f.
-#' @param msg_prefix (string, optional) A message to prefix any resulting error message.
-#' @param warn_level (integer, optional) The level of any warnings about errors encountered.
-#'   If 0 then no warnings are shown. Default: 2.
-#' @param error_level (integer, optional) The level of any resulting errors. Default: 1.
-#' @param on_error (string) The kind of message to produce if there is an error. Either "info",
-#'   "warn", or "error". Default: "error".
-#' @param simplify (boolean, optional) Whether to try to simplify the result of the mapping
-#'   into an atomic vector. Default: FALSE.
-#' @param use_names (boolean, optional) Whether to use 'x' as names in the resulting list. 'x'
-#'   must be a character vector for this to work. Default: TRUE.
+#' @param msg_prefix (string, optional) A message to prefix any resulting error
+#'   message.
+#' @param warn_level (integer, optional) The level of any warnings about errors
+#'   encountered. If 0 then no warnings are shown. Default: 2.
+#' @param error_level (integer, optional) The level of any resulting errors.
+#'   Default: 1.
+#' @param on_error (string) The kind of message to produce if there is an error.
+#'   Either "info", "warn", or "error". Default: "error".
+#' @param simplify (boolean, optional) Whether to try to simplify the result of
+#'   the mapping into an atomic vector. Default: FALSE.
+#' @param use_names (boolean, optional) Whether to use 'x' as names in the
+#'   resulting list. 'x' must be a character vector for this to work. Default:
+#'   TRUE.
 #'
-#' @return If `simplify = FALSE` a list is returned. Otherwise, the function attempts to
-#'   simplify the result to an atomic vector.
+#' @return If `simplify = FALSE` a list is returned. Otherwise, the function
+#'   attempts to simplify the result to an atomic vector.
 #'
-#' @examples
-#' \dontrun{
+#' @examples \dontrun{
+#'
 #'   test_try_pmap <- function(x, y) if (x > y) stop("x > y") else x
 #'   try_pmap(list(1:3, 3:1), test_try_pmap)
 #'   try_pmap(list(1:3, 2:4), test_try_pmap)
+#'
 #' }
 #'
 #' @export
@@ -249,8 +274,8 @@ try_pmap <- function(
   error_level = 1,
   on_error    = "error",
   simplify    = FALSE,
-  use_names   = TRUE)
-{
+  use_names   = TRUE
+) {
   if (missing(msg_prefix)) {
     mapped_function <- as.character(substitute(f))
     if (sys.nframe() > 1) {
@@ -290,7 +315,10 @@ try_pmap <- function(
       }
 
       if (warn_level > 0) {
-        warn("Failed for ", names(formals(f))[1], " = ", args[[1]], level = warn_level)
+        warn(
+          "Failed for ", names(formals(f))[1], " = ", args[[1]],
+          level = warn_level
+        )
       }
 
       if (is_scalar_character(mapped_function)) {
@@ -314,7 +342,10 @@ try_pmap <- function(
       prefix <- paste0("\n'", names(result)[is_error], "': ")
     }
 
-    error_msg <- paste0(prefix, map_chr(result[is_error], "message"), collapse = "\n")
+    error_msg <- paste0(
+      prefix, map_chr(result[is_error], "message"),
+      collapse = "\n"
+    )
     if (!is_null(msg_prefix)) {
       error_msg <- paste0(msg_prefix, "\n", error_msg)
     }
@@ -322,8 +353,9 @@ try_pmap <- function(
     switch(
       on_error,
       error = error(error_msg, level = error_level),
-      warn  = warn( error_msg, level = error_level),
-      info  = info( error_msg, level = error_level))
+      warn  = warn(error_msg,  level = error_level),
+      info  = info(error_msg,  level = error_level)
+    )
   }
 
   if (simplify && all(map_int(result, length) == 1L)) {
--- tests/testthat/test-try-catch.R
+++ tests/testthat/test-try-catch.R
@@ -1,34 +1,55 @@
-context("try catch")
-
-# TEST: try_catch -----------------------------------------------------------------------------
+# TEST: try_catch --------------------------------------------------------------
 
 test_that("try_catch catches messages, warnings and errors appropriately", {
 
   expect_error(try_catch(stop("This is an ERROR")), "This is an ERROR")
 
   expect_error(
-    capture.output(try_catch(stop("This is an ERROR"), finally = print("done"))),
-    "This is an ERROR")
+    capture.output(
+      try_catch(
+        stop("This is an ERROR"),
+        finally = print("done")
+      )
+    ),
+    "This is an ERROR"
+  )
 
   expect_output(
-    try(try_catch(stop("This is an ERROR"), finally = print("done")), silent = TRUE),
-    "done")
+    try(
+      try_catch(
+        stop("This is an ERROR"),
+        finally = print("done")
+      ),
+      silent = TRUE
+    ),
+    "done"
+  )
 
 })
 
-# TEST: try_map -------------------------------------------------------------------------------
+# TEST: try_map ----------------------------------------------------------------
 
 test_that("try_map catches errors and displays a warning", {
 
   test_try_map <- function(x, y) if (x > y) stop("x > y") else x
 
-  expect_silent(try(try_map(1:3, test_try_map, 2, warn_level = 0), silent = TRUE))
+  expect_silent(
+    try(
+      try_map(1:3, test_try_map, 2, warn_level = 0),
+      silent = TRUE
+    )
+  )
   expect_warning(
-    try(try_map(1:3, test_try_map, 2, warn_level = 1), silent = TRUE),
-    "Failed for x = 3")
+    try(
+      try_map(1:3, test_try_map, 2, warn_level = 1),
+      silent = TRUE
+    ),
+    "Failed for x = 3"
+  )
   expect_error(
     suppressWarnings(try_map(1:3, test_try_map, 2)),
-    "In test_try_map\\(\\): x > y")
+    "In test_try_map\\(\\): x > y"
+  )
 
   expect_silent(try_map(1:3, test_try_map, 5))
   ok_result <- try_map(1:3, test_try_map, 5)
@@ -37,38 +58,83 @@ test_that("try_map catches errors and displays a warning", {
   expect_identical(ok_result, as.list(1:3))
 
   expect_silent(
-    try(try_map(1:3, function(x, y) if (x > y) stop("x > y") else x, 2, warn_level = 0), silent = TRUE))
+    try(
+      try_map(
+        x = 1:3,
+        f = function(x, y) if (x > y) stop("x > y") else x,
+        2,
+        warn_level = 0
+      ),
+      silent = TRUE
+    )
+  )
+
   expect_warning(
-    try(try_map(1:3, function(x, y) if (x > y) stop("x > y") else x, 2, warn_level = 1), silent = TRUE),
-    "Failed for x = 3")
+    try(
+      try_map(
+        x = 1:3,
+        f = function(x, y) if (x > y) stop("x > y") else x,
+        2,
+        warn_level = 1
+      ),
+      silent = TRUE
+    ),
+    "Failed for x = 3"
+  )
+
   expect_error(
-    suppressWarnings(try_map(1:3, function(x, y) if (x > y) stop("x > y") else x, 2)),
-    "x > y")
+    suppressWarnings(
+      try_map(1:3, function(x, y) if (x > y) stop("x > y") else x, 2)
+    ),
+    "x > y"
+  )
+
   expect_error(
-    suppressWarnings(try_map(3, function(x, y) if (x > y) stop("x > y") else x, 2)),
-    "x > y")
+    suppressWarnings(
+      try_map(3, function(x, y) if (x > y) stop("x > y") else x, 2)
+    ),
+    "x > y"
+  )
 
   expect_warning(
     try_map(1:3, test_try_map, 2, on_error = "warn", warn_level = 0),
-    "In test_try_map\\(\\): x > y")
+    "In test_try_map\\(\\): x > y"
+  )
+
   expect_message(
     try_map(1:3, test_try_map, 2, on_error = "info", warn_level = 0),
-    "In test_try_map\\(\\): x > y")
+    "In test_try_map\\(\\): x > y"
+  )
+
 })
 
-# TEST: try_pmap ------------------------------------------------------------------------------
+# TEST: try_pmap ---------------------------------------------------------------
 
 test_that("try_pmap catches errors and displays a warning", {
 
   test_try_pmap <- function(x, y) if (x > y) stop("x > y") else x
 
-  expect_silent(try(try_pmap(list(1:3, 3:1), test_try_pmap, warn_level = 0), silent = TRUE))
+  expect_silent(
+    try(
+      try_pmap(list(1:3, 3:1), test_try_pmap, warn_level = 0),
+      silent = TRUE
+    )
+  )
+
   expect_warning(
-    try(try_pmap(list(1:3, 3:1), test_try_pmap, warn_level = 1), silent = TRUE),
-    "Failed for x = 3")
+    try(
+      try_pmap(list(1:3, 3:1), test_try_pmap, warn_level = 1),
+      silent = TRUE
+    ),
+    "Failed for x = 3"
+  )
+
   expect_error(
-    suppressWarnings(try_pmap(list(1:3, 3:1), test_try_pmap)),
-    "In test_try_pmap\\(\\): x > y")
+    suppressWarnings(
+      try_pmap(list(1:3, 3:1), test_try_pmap)
+    ),
+    "In test_try_pmap\\(\\): x > y"
+  )
 
   expect_silent(try_pmap(list(1:3, 2:4), test_try_pmap))
   ok_result <- try_pmap(list(1:3, 2:4), test_try_pmap)
@@ -77,22 +143,50 @@ test_that("try_pmap catches errors and displays a warning", {
   expect_identical(ok_result, as.list(1:3))
 
   expect_silent(
-    try(try_pmap(list(1:3, 3:1), function(x, y) if (x > y) stop("x > y") else x, warn_level = 0), silent = TRUE))
+    try(
+      try_pmap(
+        l = list(1:3, 3:1),
+        f = function(x, y) if (x > y) stop("x > y") else x,
+        warn_level = 0
+      ),
+      silent = TRUE
+    )
+  )
+
   expect_warning(
-    try(try_pmap(list(1:3, 3:1), function(x, y) if (x > y) stop("x > y") else x, warn_level = 1), silent = TRUE),
-    "Failed for x = 3")
+    try(
+      try_pmap(
+        l = list(1:3, 3:1),
+        f = function(x, y) if (x > y) stop("x > y") else x,
+        warn_level = 1
+      ),
+      silent = TRUE
+    ),
+    "Failed for x = 3"
+  )
+
   expect_error(
-    suppressWarnings(try_pmap(list(1:3, 3:1), function(x, y) if (x > y) stop("x > y") else x)),
-    "x > y")
+    suppressWarnings(
+      try_pmap(list(1:3, 3:1), function(x, y) if (x > y) stop("x > y") else x)
+    ),
+    "x > y"
+  )
+
   expect_error(
-    suppressWarnings(try_pmap(list(3, 1), function(x, y) if (x > y) stop("x > y") else x)),
-    "x > y")
+    suppressWarnings(
+      try_pmap(list(3, 1), function(x, y) if (x > y) stop("x > y") else x)
+    ),
+    "x > y"
+  )
 
   expect_warning(
     try_pmap(list(1:3, 3:1), test_try_pmap, on_error = "warn", warn_level = 0),
-    "In test_try_pmap\\(\\): x > y")
+    "In test_try_pmap\\(\\): x > y"
+  )
+
   expect_message(
     try_pmap(list(1:3, 3:1), test_try_pmap, on_error = "info", warn_level = 0),
-    "In test_try_pmap\\(\\): x > y")
+    "In test_try_pmap\\(\\): x > y"
+  )
 
 })
