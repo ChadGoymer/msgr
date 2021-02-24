9b93cecf0797db47d25dfa8f530b73b03e71aab5
Chad Goymer <chad.goymer@gmail.com>
2021-02-24 11:43:45 +0000

    Vectorised predicates using {fs} for cehcking files

--- R/predicates.R
+++ R/predicates.R
@@ -27,6 +27,9 @@ is_na <- function(x) {
 #
 #' Checks whether the variable is a valid URL
 #'
+#' If a character vector is supplied each element is checked, returning a
+#' logical vector of the same length.
+#'
 #' @param x (any) The object to test
 #'
 #' @return TRUE if x is a valid URL, FALSE otherwise
@@ -34,6 +37,7 @@ is_na <- function(x) {
 #' @examples
 #' is_url("http://something.com")
 #' is_url("https://google.com")
+#' is_url(c("http://something.com", "https://google.com"))
 #'
 #' is_url(1)
 #' is_url("foo")
@@ -42,12 +46,20 @@ is_na <- function(x) {
 #' @export
 #'
 is_url <- function(x) {
-  is_scalar_character(x) && grepl("^(https|http)://", x)
+  if (is_character(x)) {
+    grepl("^(https|http)://", x)
+  } else {
+    FALSE
+  }
 }
 
 #  FUNCTION: is_dir ------------------------------------------------------------
 #
-#' Checks whether the variable is a path to an existing directory
+#' Check file paths and directories
+#'
+#' These functions check whether the directories or files exist and whether the
+#' current user has read or write permissions. If a character vector is supplied
+#' each element is checked, returning a logical vector of the same length.
 #'
 #' @param x (any) The object to test
 #'
@@ -56,93 +68,79 @@ is_url <- function(x) {
 #' @examples
 #' is_dir(tempdir())
 #' is_dir("/does/not/exist")
-#' is_dir(1)
 #'
-#' @export
-#'
-is_dir <- function(x) {
-  is_scalar_character(x) && dir.exists(x)
-}
-
-#  FUNCTION: is_file -----------------------------------------------------------
-#
-#' Checks whether the variable is a path to an existing file
-#'
-#' @param x (any) The object to test
-#'
-#' @return TRUE if x is a path to an existing file, FALSE otherwise
-#'
-#' @examples
 #' tmpfile <- tempfile()
 #' file.create(tmpfile)
 #'
 #' is_file(tmpfile)
-#'
 #' is_file("/does/not/exist.txt")
-#' is_file(1)
+#'
+#' is_readable(tmpfile)
+#' is_readable("/does/not/exist.txt")
+#'
+#' is_writeable(tmpfile)
+#' is_writeable("/does/not/exist.txt")
 #'
 #' @export
 #'
+is_dir <- function(x) {
+  if (is_character(x)) {
+    unname(fs::is_dir(x))
+  } else {
+    FALSE
+  }
+}
+
+#  FUNCTION: is_file -----------------------------------------------------------
+#
+#' @rdname is_dir
+#' @export
+#'
 is_file <- function(x) {
-  is_scalar_character(x) && file.exists(x) && !file.info(x)$isdir
+  if (is_character(x)) {
+    unname(fs::is_file(x))
+  } else {
+    FALSE
+  }
 }
 
 #  FUNCTION: is_readable -------------------------------------------------------
 #
-#' Checks whether input is a path to an existing, readable file or directory
-#'
-#' @param x (any) The object to test
-#'
-#' @return TRUE if x is a path to an existing, readable file or directory, FALSE
-#'   otherwise
-#'
-#' @examples
-#' tmpfile <- tempfile()
-#' file.create(tmpfile)
-#'
-#' is_readable(tmpfile)
-#'
-#' is_readable("/does/not/exist.txt")
-#' is_readable(1)
-#'
+#' @rdname is_dir
 #' @export
 #'
 is_readable <- function(x) {
-  is_scalar_character(x) && file.exists(x) && file.access(x, mode = 4)[[1]] == 0
+  if (is_character(x)) {
+    unname(fs::file_access(x, mode = "read"))
+  } else {
+    FALSE
+  }
 }
 
 #  FUNCTION: is_writeable ------------------------------------------------------
 #
-#' Checks whether input is a path to an existing, writeble file or directory
-#'
-#' @param x (any) The object to test
-#'
-#' @return TRUE if x is a path to an existing, writeable file or directory,
-#'   FALSE otherwise
-#'
-#' @examples
-#' tmpfile <- tempfile()
-#' file.create(tmpfile)
-#'
-#' is_writeable(tmpfile)
-#'
-#' is_writeable("/does/not/exist.txt")
-#' is_writeable(1)
-#'
+#' @rdname is_dir
 #' @export
 #'
 is_writeable <- function(x) {
-  is_scalar_character(x) && file.exists(x) && file.access(x, mode = 2)[[1]] == 0
+  if (is_character(x)) {
+    unname(fs::file_access(x, mode = "write"))
+  } else {
+    FALSE
+  }
 }
 
 #  FUNCTION: is_in -------------------------------------------------------------
 #
-#' Checks whether all elements of one variable are in another
+#' Checks whether elements of one variable are in another
+#'
+#' If `x` is a character vector each element is checked, returning a logical
+#' vector of the same length.
 #'
 #' @param x (any) The object with elements to test
 #' @param y (any) The object with elements to test against
 #'
-#' @return TRUE if all elements in x are in y, FALSE otherwise
+#' @return TRUE if elements in x are in y, FALSE otherwise
 #'
 #' @examples
 #' is_in("a", letters)
@@ -154,18 +152,21 @@ is_writeable <- function(x) {
 #' @export
 #'
 is_in <- function(x, y) {
-  all(x %in% y)
+  x %in% y
 }
 
 #  FUNCTION: has_names ---------------------------------------------------------
 #
 #' Checks whether the variable has names
 #'
+#' If `nm` is a character vector each element is checked, returning a logical
+#' vector of the same length.
+#'
 #' @param x (any) The object to test
 #' @param nm (character, optional) The names to check for. If not specified then
 #'   the function checks for any names.
 #'
-#' @return TRUE if x has any names, FALSE otherwise
+#' @return TRUE if x has names, FALSE otherwise
 #'
 #' @examples
 #' x <- list(a = 1, b = 2)
--- tests/testthat/test-predicates.R
+++ tests/testthat/test-predicates.R
@@ -43,6 +43,10 @@ test_that("is_url returns TRUE for valid URLs", {
 
   expect_true(is_url("https://www.secure.com"))
   expect_true(is_url("http://www.insecure.com"))
+  expect_identical(
+    is_url(c("http://www.insecure.com", "http://www.insecure.com")),
+    c(TRUE, TRUE)
+  )
 
   expect_true(is_url("https://www.secure.com/some/endpoint"))
   expect_true(is_url("http://www.insecure.com/some/endpoint"))
@@ -58,11 +62,9 @@ test_that("is_url returns FALSE for invalid URLs", {
   expect_false(is_url(matrix(1:4, nrow = 2)))
   expect_false(is_url(data.frame(x = LETTERS[1:3], y = 1:3)))
 
-  expect_false(
-    is_url(c(
-      "https://www.secure.com/endpoint1",
-      "https://www.secure.com/endpoint2"
-    ))
+  expect_identical(
+    is_url(c("https://www.secure.com/endpoint","bob")),
+    c(TRUE, FALSE)
   )
 
 })
@@ -71,20 +73,38 @@ test_that("is_url returns FALSE for invalid URLs", {
 
 test_that("is_dir returns TRUE for an existing directory", {
 
+  home_dir <- R.home()
   temp_dir <- tempdir()
 
+  expect_true(is_dir(home_dir))
   expect_true(is_dir(temp_dir))
+
+  expect_identical(
+    is_dir(c(home_dir, temp_dir)),
+    c(TRUE, TRUE)
+  )
 
 })
 
 test_that("is_dir returns FALSE for an invalid directory", {
 
   temp_dir <- tempdir()
-  no_dir <- file.path(temp_dir, "does_not_exist")
+  no_dir   <- file.path(temp_dir, "does_not_exist")
+
+  expect_false(is_dir(TRUE))
+  expect_false(is_dir(3.142))
+  expect_false(is_dir("bob"))
+  expect_false(is_dir(list(1:3)))
+  expect_false(is_dir(matrix(1:4, nrow = 2)))
+  expect_false(is_dir(data.frame(x = LETTERS[1:3], y = 1:3)))
 
   expect_false(is_dir("C:/does/not/exist"))
   expect_false(is_dir(no_dir))
-  expect_false(is_dir(c(temp_dir, temp_dir)))
+
+  expect_identical(
+    is_dir(c(temp_dir, no_dir)),
+    c(TRUE, FALSE)
+  )
 
 })
 
@@ -92,22 +112,43 @@ test_that("is_dir returns FALSE for an invalid directory", {
 
 test_that("is_file returns TRUE for an existing directory", {
 
-  temp_file <- tempfile(fileext = ".txt")
-  file.create(temp_file)
+  temp_file_1 <- tempfile("1", fileext = ".txt")
+  temp_file_2 <- tempfile("2", fileext = ".txt")
 
-  expect_true(is_file(temp_file))
+  file.create(temp_file_1)
+  file.create(temp_file_2)
+
+  expect_true(is_file(temp_file_1))
+  expect_true(is_file(temp_file_2))
+
+  expect_identical(
+    is_file(c(temp_file_1, temp_file_2)),
+    c(TRUE, TRUE)
+  )
 
 })
 
 test_that("is_file returns FALSE for an invalid directory", {
 
   temp_file <- tempfile(fileext = ".txt")
-  file.create(temp_file)
+
   no_file <- file.path(tempdir(), "does_not_exist.txt")
+  file.create(temp_file)
+
+  expect_false(is_file(TRUE))
+  expect_false(is_file(3.142))
+  expect_false(is_file("bob"))
+  expect_false(is_file(list(1:3)))
+  expect_false(is_file(matrix(1:4, nrow = 2)))
+  expect_false(is_file(data.frame(x = LETTERS[1:3], y = 1:3)))
 
   expect_false(is_file("C:/does/not/exist.txt"))
   expect_false(is_file(no_file))
-  expect_false(is_file(c(temp_file, temp_file)))
+
+  expect_identical(
+    is_file(c(temp_file, no_file)),
+    c(TRUE, FALSE)
+  )
 
 })
 
@@ -123,6 +164,11 @@ test_that("is_readable returns TRUE for a readable file or directory", {
   expect_true(is_readable(temp_dir))
   expect_true(is_readable(temp_file))
 
+  expect_identical(
+    is_readable(c(temp_dir, temp_file)),
+    c(TRUE, TRUE)
+  )
+
 })
 
 test_that("is_readable returns FALSE for an unreadable file or directory", {
@@ -132,9 +178,21 @@ test_that("is_readable returns FALSE for an unreadable file or directory", {
 
   temp_file <- file.path(temp_dir, "unreadable_file.txt")
 
+  expect_false(is_readable(TRUE))
+  expect_false(is_readable(3.142))
+  expect_false(is_readable("bob"))
+  expect_false(is_readable(list(1:3)))
+  expect_false(is_readable(matrix(1:4, nrow = 2)))
+  expect_false(is_readable(data.frame(x = LETTERS[1:3], y = 1:3)))
+
   expect_false(is_readable("C:/does/not/exist"))
   expect_false(is_readable(no_dir))
   expect_false(is_readable(temp_file))
+
+  expect_identical(
+    is_readable(c(temp_dir, no_dir)),
+    c(TRUE, FALSE)
+  )
 
 })
 
@@ -150,6 +208,11 @@ test_that("is_writeable returns TRUE for a readable file or directory", {
   expect_true(is_writeable(temp_dir))
   expect_true(is_writeable(temp_file))
 
+  expect_identical(
+    is_readable(c(temp_dir, temp_file)),
+    c(TRUE, TRUE)
+  )
+
 })
 
 test_that("is_writeable returns FALSE for an unreadable file or directory", {
@@ -159,26 +222,47 @@ test_that("is_writeable returns FALSE for an unreadable file or directory", {
 
   temp_file <- file.path(temp_dir, "unwriteable_file.txt")
 
+  expect_false(is_writeable(TRUE))
+  expect_false(is_writeable(3.142))
+  expect_false(is_writeable("bob"))
+  expect_false(is_writeable(list(1:3)))
+  expect_false(is_writeable(matrix(1:4, nrow = 2)))
+  expect_false(is_writeable(data.frame(x = LETTERS[1:3], y = 1:3)))
+
   expect_false(is_writeable("C:/does/not/exist"))
   expect_false(is_writeable(no_dir))
   expect_false(is_writeable(temp_file))
+
+  expect_identical(
+    is_readable(c(temp_dir, no_dir)),
+    c(TRUE, FALSE)
+  )
 
 })
 
 # TEST: is_in ------------------------------------------------------------------
 
-test_that("is_in returns TRUE if all elements of x are in y", {
+test_that("is_in returns TRUE if elements of x are in y", {
 
   expect_true(is_in(1, 1:3))
-  expect_true(is_in(c("a", "b"), letters))
-  expect_true(is_in(list("a", "b"), as.list(letters)))
+  expect_identical(
+    is_in(c("a", "b"), letters),
+    c(TRUE, TRUE)
+  )
+  expect_identical(
+    is_in(list("a", "b"), as.list(letters)),
+    c(TRUE, TRUE)
+  )
 
 })
 
-test_that("is_in returns FALSE if all the elements of x are not in y", {
+test_that("is_in returns FALSE if the elements of x are not in y", {
 
   expect_false(is_in(0, 1:3))
-  expect_false(is_in(LETTERS, c("A", "B", "C")))
+  expect_identical(
+    is_in(c("A", "D"), c("A", "B", "C")),
+    c(TRUE, FALSE)
+  )
 
 })
 
@@ -191,7 +275,10 @@ test_that("has_names returns TRUE for an object with names", {
   expect_true(has_names(data.frame(a = 1, b = 2)))
 
   expect_true(has_names(c(a = 1, b = 2), "a"))
-  expect_true(has_names(list(a = 1, b = 2), c("a", "b")))
+  expect_identical(
+    has_names(list(a = 1, b = 2), c("a", "b")),
+    c(TRUE, TRUE)
+  )
   expect_true(has_names(data.frame(a = 1, b = 2), "a"))
 
 })
@@ -203,11 +290,15 @@ test_that("has_names returns FALSE for an object without names", {
   expect_false(has_names(list(1:3)))
 
   expect_false(has_names(c(a = 1, b = 2), "c"))
-  expect_false(has_names(list(a = 1, b = 2), c("b", "c")))
   expect_false(has_names(data.frame(a = 1, b = 2), "c"))
 
   expect_false(has_names(1:3, "a"))
   expect_false(has_names("bob", "a"))
   expect_false(has_names(list(1:3), "a"))
+
+  expect_identical(
+    has_names(list(a = 1, b = 2), c("b", "c")),
+    c(TRUE, FALSE)
+  )
 
 })
